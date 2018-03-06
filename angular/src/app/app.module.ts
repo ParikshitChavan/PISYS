428e7a25cc881954e4a6b02ec940a242801efd14
Parikshit Chavan <parikshitschavan@gmail.com>
2018-03-06 13:56:26 +0900

    Added Materialize and created 2 components

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { MaterializeModule } from 'angular2-materialize';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
